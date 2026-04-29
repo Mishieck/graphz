9d6979bc0c7f4f29e017e7ac7844497772270978
Mishieck Mwale <mishieckdev@gmail.com>
2026-04-29 18:52:13 +0200

    feat: create inner types
    Create inner types for structs. Make the types public.

--- src/root.zig
+++ src/root.zig
@@ -6,17 +6,20 @@ pub const Edge = [2]usize;
 pub fn Graph(comptime Data: type) type {
     return struct {
         const Self = @This();
+        pub const Nodes = ArrayList(Data);
+        pub const Edges = ArrayList(Edge);
 
-        nodes: ArrayList(Data),
-        edges: ArrayList(Edge),
+        nodes: Nodes,
+        edges: Edges,
     };
 }
 
 pub fn Node(comptime Data: type) type {
     return struct {
         const Self = @This();
+        pub const Neighbors = ArrayList(*Self);
 
         data: Data,
-        neighbors: ArrayList(*Self),
+        neighbors: Neighbors,
     };
 }
