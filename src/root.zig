c41e118810d364004081fa1c7b1091ad7b64ce5f
Mishieck Mwale <mishieckdev@gmail.com>
2026-05-10 18:47:40 +0200

    feat: update tree-node and traversal
    - Let `TreeNode` be an abstraction over `Node`.
    - Use a skipper in traversal to prevent cyclic iteration.

--- src/root.zig
+++ src/root.zig
@@ -28,55 +28,116 @@ pub fn Node(comptime Data: type) type {
             self: *Self,
             arena: mem.Allocator,
             comptime traversal: Traversal.Method,
-        ) !Traversal.Iterator(traversal, Self, getNeighbors) {
-            return try Traversal.traverse(arena, Self, traversal, self, getNeighbors);
+            comptime skipper: anytype,
+        ) !Traversal.Iterator(traversal, Data, skipper) {
+            return try Traversal.traverse(arena, Data, traversal, self, skipper);
         }
 
-        pub fn getNeighbors(self: *const Self) Neighbors {
-            return self.neighbors;
+        pub const default_skipper = Traversal.Skipper(Data, void){
+            .record = {},
+            .skip = dont_skip,
+        };
+
+        pub fn dont_skip(
+            self: Traversal.Skipper(Data, void),
+            node: *const Self,
+            index: usize,
+            neighbors: Neighbors,
+        ) bool {
+            _ = self;
+            _ = node;
+            _ = index;
+            _ = neighbors;
+            return false;
         }
     };
 }
+
 pub fn TreeNode(comptime Data: type) type {
     return struct {
-        const Self = @This();
-        pub const Children = ArrayList(*Self);
+        const Self = Node(Data);
 
-        data: Data,
-        parent: ?*Self,
-        children: Children,
+        pub fn init(arena: mem.Allocator, data: Data, parent_node: ?*Self) !Self {
+            const neighbors = Self.Neighbors.init(arena);
+            var node = Self{
+                .data = data,
+                .neighbors = neighbors,
+            };
+
+            try node.neighbors.append(parent_node orelse self: {
+                const n = try arena.create(Self);
+                n.* = node;
+                break :self n;
+            });
+
+            return node;
+        }
 
         pub fn traverse(
             self: *Self,
             arena: mem.Allocator,
             comptime traversal: Traversal.Method,
-        ) !Traversal.Iterator(traversal, Self, getChildren) {
-            return try Traversal.traverse(arena, Self, traversal, self, getChildren);
+        ) !Traversal.Iterator(traversal, Data, parent_skipper) {
+            return try Traversal.traverse(arena, Data, traversal, self, parent_skipper);
         }
 
-        pub fn getChildren(self: *const Self) Children {
-            return self.children;
+        pub const parent_skipper = Traversal.Skipper(Data, void){
+            .record = {},
+            .skip = skipParent,
+        };
+
+        pub fn skipParent(
+            self: Traversal.Skipper(Data, void),
+            node: *const Self,
+            index: usize,
+            neighbors: Self.Neighbors,
+        ) bool {
+            _ = self;
+            _ = node;
+            _ = neighbors;
+            return index == 0;
+        }
+
+        pub fn parent(self: *const Self) ?*const Self {
+            const p = self.neighbors.items[0];
+            return if (p == self) null else p;
         }
 
         pub fn previousSibling(self: *const Self) ?*Self {
-            return self.getSibling(-1);
+            return getSibling(self, -1);
         }
 
         pub fn nextSibling(self: *const Self) ?*Self {
-            return self.getSibling(1);
+            return getSibling(self, 1);
+        }
+
+        pub fn children(self: *const Self) Children {
+            return .{ .neighbors = self.neighbors };
         }
 
         pub fn getSibling(self: *const Self, offset: isize) ?*Self {
-            return if (self.parent) |parent| sibling: {
-                const node_position: isize = for (parent.children.items, 0..) |child, i| {
+            return if (parent(self)) |p| sibling: {
+                const node_position: isize = for (p.neighbors.items, 0..) |child, i| {
                     if (child == self) break @bitCast(i);
                 } else break :sibling null;
 
                 const sibling_position = node_position + offset;
-                const is_within_range = sibling_position >= 0 and sibling_position < parent.children.items.len;
-                break :sibling if (is_within_range) parent.children.items[@bitCast(sibling_position)] else null;
+                const is_within_range = sibling_position > 0 and sibling_position < p.neighbors.items.len;
+                break :sibling if (is_within_range) p.neighbors.items[@bitCast(sibling_position)] else null;
             } else null;
         }
+
+        pub const Children = struct {
+            neighbors: Self.Neighbors,
+            current_index: usize = 1,
+
+            pub fn next(self: *Children) ?*Self {
+                if (self.current_index == self.neighbors.items.len) return null;
+                const node = self.neighbors.items[self.current_index];
+                self.current_index += 1;
+                return node;
+            }
+        };
     };
 }
 
@@ -86,16 +147,25 @@ test TreeNode {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var node = N{ .data = 0, .parent = null, .children = .init(allocator) };
-    var left = N{ .data = 1, .parent = &node, .children = .init(allocator) };
-    try node.children.append(&left);
-    var center = N{ .data = 1, .parent = &node, .children = .init(allocator) };
-    try node.children.append(&center);
-    var right = N{ .data = 2, .parent = &node, .children = .init(allocator) };
-    try node.children.append(&right);
+    var node = try N.init(allocator, 0, null);
+    var left = try N.init(allocator, 1, &node);
+    try node.neighbors.append(&left);
+    var center = try N.init(allocator, 2, &node);
+    try node.neighbors.append(&center);
+    var right = try N.init(allocator, 3, &node);
+    try node.neighbors.append(&right);
+    var left_of_left = try N.init(allocator, 4, &node);
+    try left.neighbors.append(&left_of_left);
 
-    try testing.expectEqual(&left, center.previousSibling());
-    try testing.expectEqual(&right, center.nextSibling());
+    try testing.expectEqual(&left, N.previousSibling(&center));
+    try testing.expectEqual(&right, N.nextSibling(&center));
+
+    var it = try N.traverse(&node, allocator, .level_order);
+    for ([_]u8{ 0, 1, 2, 3 }) |expected| {
+        const actual = try it.next();
+        try testing.expect(actual != null);
+        try testing.expectEqual(expected, actual.?.data);
+    }
 }
 
 pub const Traversal = struct {
@@ -106,39 +176,42 @@ pub const Traversal = struct {
         _,
     };
 
-    pub fn NeighborGetter(comptime N: type) type {
-        return fn (node: *const N) ArrayList(*N);
-    }
-
     pub fn traverse(
         arena: mem.Allocator,
-        comptime N: type,
+        comptime Data: type,
         comptime traversal: Traversal.Method,
-        node: *N,
-        comptime getNeighbors: *const NeighborGetter(N),
-    ) !Iterator(traversal, N, getNeighbors) {
+        node: *Node(Data),
+        comptime skipper: anytype,
+    ) !Iterator(traversal, Data, skipper) {
+        const N = Node(Data);
         var nodes = ArrayList(*const N).init(arena);
         try nodes.append(node);
 
         return switch (traversal) {
-            .level_order => Iterator(traversal, N, getNeighbors){ .nodes = nodes },
-            .post_order => Iterator(traversal, N, getNeighbors){
+            .level_order => Iterator(traversal, Data, skipper){ .nodes = nodes },
+            .post_order => Iterator(traversal, Data, skipper){
                 .nodes = nodes,
                 .processed = .init(arena),
             },
-            .pre_order => Iterator(traversal, N, getNeighbors){ .nodes = nodes },
+            .pre_order => Iterator(traversal, Data, skipper){ .nodes = nodes },
             _ => unreachable,
         };
     }
 
+    pub fn SkipperRecord(skipper: anytype) type {
+        return @typeInfo(@TypeOf(skipper)).@"struct".fields[0].type;
+    }
+
     pub fn Iterator(
         comptime traversal: Traversal.Method,
-        N: type,
-        comptime getNeighbors: *const NeighborGetter(N),
+        Data: type,
+        comptime skipper: anytype,
     ) type {
+        const N = Node(Data);
+        const SR = SkipperRecord(skipper);
+        const sk: Skipper(Data, SR) = skipper;
+
         // Iterator for level-order traversal.
-        //
-        // > [!WARNING] If the graph contains cycles, look out for duplicates!
         const LevelOrderTraversal = struct {
             const It = @This();
 
@@ -147,14 +220,18 @@ pub const Traversal = struct {
             pub fn next(self: *It) !?*const N {
                 if (self.nodes.items.len == 0) return null;
                 const head = self.nodes.orderedRemove(0);
-                try self.nodes.appendSlice(getNeighbors(head).items);
+
+                var i: usize = 0;
+                for (head.neighbors.items) |neighbor| {
+                    if (!sk.skip(sk, head, i, head.neighbors)) try self.nodes.append(neighbor);
+                    i += 1;
+                }
+
                 return head;
             }
         };
 
         // Iterator for post-order traversal.
-        //
-        // > [!WARNING] If the graph contains cycles, look out for duplicates!
         const PostOrderTraversal = struct {
             const It = @This();
 
@@ -165,16 +242,17 @@ pub const Traversal = struct {
                 if (self.nodes.items.len == 0) return null;
                 const head = self.nodes.getLast();
                 if (self.processed.get(head)) |_| return self.nodes.pop().?;
-                var it = mem.reverseIterator(getNeighbors(head).items);
-                while (it.next()) |n| try self.nodes.append(n);
+                var it = mem.reverseIterator(head.neighbors.items);
+                var i: usize = 0;
+                while (it.next()) |n| : (i += 1) {
+                    if (!sk.skip(sk, n, i, head.neighbors)) try self.nodes.append(n);
+                }
                 try self.processed.put(head, head);
                 return self.next();
             }
         };
 
         // Iterator for pre-order traversal.
-        //
-        // > [!WARNING] If the graph contains cycles, look out for duplicates!
         const PreOrderTraversal = struct {
             const It = @This();
 
@@ -183,8 +261,13 @@ pub const Traversal = struct {
             pub fn next(self: *It) !?*const N {
                 if (self.nodes.items.len == 0) return null;
                 const head = self.nodes.pop().?;
-                var it = mem.reverseIterator(getNeighbors(head).items);
-                while (it.next()) |n| try self.nodes.append(n);
+
+                var it = mem.reverseIterator(head.neighbors.items);
+                var i: usize = 0;
+                while (it.next()) |n| : (i += 1) {
+                    if (!sk.skip(sk, n, i, head.neighbors)) try self.nodes.append(n);
+                }
+
                 return head;
             }
         };
@@ -194,6 +277,17 @@ pub const Traversal = struct {
             .post_order => PostOrderTraversal,
             .pre_order => PreOrderTraversal,
             _ => unreachable,
+        };
+    }
+
+    pub fn Skipper(Data: type, Record: type) type {
+        const N = Node(Data);
+
+        return struct {
+            const Skip = @This();
+
+            record: Record,
+            skip: *const fn (self: Skip, node: *const N, index: usize, neighbors: N.Neighbors) bool,
         };
     }
 };
@@ -222,7 +316,8 @@ fn testTraverse(comptime method: Traversal.Method, expectations: []const u8) !vo
     var left_of_left = N{ .data = 3, .neighbors = .init(allocator) };
     try left.neighbors.append(&left_of_left);
 
-    var it = try node.traverse(allocator, method);
+    const skipper = N.default_skipper;
+    var it = try node.traverse(allocator, method, skipper);
     for (expectations) |expected| {
         const actual = try it.next();
         try testing.expect(actual != null);
