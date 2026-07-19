593a5ffb6ea6af063007a084db1941058ac6fe62
Mishieck Mwale <mishieckdev@gmail.com>
2026-07-19 11:34:19 +0200

    test(root): add tests to root module

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,9 @@
 pub const graph = @import("graph.zig");
 pub const tree = @import("tree.zig");
 pub const Traversal = @import("traversal.zig").Traversal;
+
+test {
+    _ = graph;
+    _ = tree;
+    _ = Traversal;
+}
